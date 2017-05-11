8073a23b2d7375a876a21b0f6e86ba5105b5557e
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-05-11 16:17:45 +0200

    Change Dummy plugin setup to internal  class

--- Dummy/src/DummyPlugin.cpp
+++ Dummy/src/DummyPlugin.cpp
@@ -1,28 +1,24 @@
-#include "DummyPlugin.h"
-
+#include <VREPPlugin.h>
 #include <Log.h>
 
-unsigned char DummyPlugin::version() const
-{
-  return 1;
-}
+class DummyPlugin: public VREPPlugin {
+  public:
+    DummyPlugin() {}
+    DummyPlugin& operator=(const DummyPlugin&) = delete;
+    DummyPlugin(const DummyPlugin&) = delete;
+    virtual ~DummyPlugin() {}
+    virtual unsigned char version() const { return 1; }
+    virtual bool load() {
+      Log::name(name());
+      Log::out() << "loaded" << std::endl;
+      return true;
+    }
+    virtual bool unload() {
 
-const std::string DummyPlugin::name() const
-{
-  return "Dummy Plugin";
-}
-
-bool DummyPlugin::load()
-{
-  Log::name(name());
-  Log::out() << "loaded" << std::endl;
-  return true;
-}
-
-bool DummyPlugin::unload()
-{
-  Log::out() << "unloaded" << std::endl;
-  return true;
-}
-
-DummyPlugin plugin;
+      Log::out() << "unloaded" << std::endl;
+      return true;
+    }
+    virtual const std::string name() const { 
+      return "Dummy Plugin"; 
+    }
+} plugin;
